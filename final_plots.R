c621e60fec59d25cd6d1cffe1189743cedb68cd9
piklprado <piklprado@gmail.com>
2018-02-14 22:33:42 -0200

    many changes: carnaval revision

--- final_plots.R
+++ final_plots.R
@@ -8,8 +8,8 @@ load("simulations/anovaresults.RData")
 load("simulations/lmresults.RData")
 
 ## Plot Parameters (from http://shinyapps.org/apps/RGraphCompendium/)
-par(cex.main = 1.5, mar = c(5, 6, 4, 4) + 0.1, mgp = c(3.5, 1, 0), 
-    cex.lab = 1.5, font.lab = 2, cex.axis = 1.3, bty = "n", las = 1)
+#par(cex.main = 1.5, mar = c(5, 6, 4, 4) + 0.1, mgp = c(3.5, 1, 0), 
+#    cex.lab = 1.5, font.lab = 2, cex.axis = 1.3, bty = "n", las = 1)
 greycol <- rgb(red = 190, green = 190, blue = 190, alpha = 95, maxColorValue = 255)
 greycol2 <- rgb(0, 0, 0, alpha = 0.7)
 grey1 <- grey.colors(10, alpha=0.3)[1]
@@ -51,14 +51,14 @@ par(cex.main = 1.5, lwd=2,
     cex.lab = 1.25, font.lab = 2, cex.axis = 1.2, bty = "l", las = 1, mfrow=c(2,2),
     oma=c(3,3,0,0))
 p3b(t.results, main = "t-test", AIC2=FALSE, colours=c("black","grey","grey"),
-    legend=FALSE, xlab="", ylab="", cex=0.25, xlim=c(0.1,3.5), log="xy")
+    legend=FALSE, xlab="", ylab="", cex=0.25, xlim=c(0.1,2))
 legend("topright",c("NHT","IT"), pch=19, col=c("black","darkgrey"), bty="n", cex=1.2)
 p3b(cor.results, main = "Correlation", AIC2=FALSE, colours=c("black","grey","grey"),
-    legend=FALSE, xlab="", ylab="", cex=0.25,  xlim=c(0.1,3.5), log="xy")
+    legend=FALSE, xlab="", ylab="", cex=0.25,  xlim=c(0.1,2))
 p3b(anova.results, main = "ANOVA", AIC1=FALSE, colours=c("black","grey","grey"),
-    legend=FALSE, xlab="", ylab="", cex=0.25,  xlim=c(0.1,3.5))
+    legend=FALSE, xlab="", ylab="", cex=0.25,  xlim=c(0.1,2))
 p3b(lm.results, main = "Linear regression", AIC1=FALSE, colours=c("black","grey","grey"),
-    legend=FALSE, xlab="", ylab="", cex=0.25,  xlim=c(0.1,3.5), log="xy")
+    legend=FALSE, xlab="", ylab="", cex=0.25,  xlim=c(0.1,2))
 mtext("Effect Size", side=1, outer=TRUE, cex=2, line=0)
 mtext("Mean M-error rate", side=2, outer=TRUE, cex=2, line=0.5, las=0)
 dev.off()
@@ -71,7 +71,7 @@ par(cex.main = 1.5, lwd=2,
     cex.lab = 1.25, font.lab = 2, cex.axis = 1.2, bty = "l", las = 1, mfrow=c(2,2),
     oma=c(3,3,0,0))
 p4b(t.results, main = "t-test", AIC2=FALSE, colours=c("black","grey","grey"),
-    legend=FALSE, xlab="", ylab="", cex=0.25, xlim=c(0.1,1.5))#, log="xy")
+    legend=FALSE, xlab="", ylab="", cex=0.25, xlim=c(0.1,1.5))
 legend("topright",c("NHT","IT"), pch=19, col=c("black","darkgrey"), bty="n", cex=1.2)
 p4b(cor.results, main = "Correlation", AIC2=FALSE, colours=c("black","grey","grey"),
     legend=FALSE, xlab="", ylab="", cex=0.25,  xlim=c(0.1,1.5))
@@ -116,7 +116,7 @@ dev.off()
 ## Minimum value of the likelihhod-ratio to reject H0 expressed in units of t (expression 9.5.3 from Edawrds, 1972)
 st <- function(n, m) sqrt((n-1)*(exp(2*m/n)-1))
 ## Critical t for a certain significance value, from the t distribution
-cp <- function(n, alpha) qt(p= 1-alfa/2, df = n-1)
+cp <- function(n, alpha) qt(p= 1-alpha/2, df = n-1)
 ## Fig S1
 pdf("../overleaf/figures/figS1.pdf")
 par(cex.axis=1.5, cex.lab=1.75, las=1, mar = c(5, 6, 4, 2), mgp = c(4,1,0), lwd=1.5)
@@ -135,7 +135,6 @@ pdf("../overleaf/figures/weights_and_ttest.pdf", width=8, height=6.5)
 par(cex= 1.25, lwd=2)
 #par(cex.main = 1.5, mar = c(5, 6, 4, 4) + 0.1, mgp = c(3.5, 1, 0), 
 #    cex.lab = 1.5, font.lab = 2, cex.axis = 1.3, cex=2)
-
 plot(x, y1, type="n", axes=FALSE, xlab="", ylab="")
 polygon(c(a[1],x[x>a[1]]), c(0,y1[x>a[1]]), col=grey1, lty=0)
 polygon(c(a[2],x[x>a[2]]), c(0,y1[x>a[2]]), col=grey2, lty=0)
@@ -155,20 +154,17 @@ dev.off()
 
 
 ## P rightfull conclusions for both IT criteria
-pdf("../overleaf/figures/Sup_prightH1.pdf", width=8, height=11)
+pdf("../overleaf/figures/Sup_prightH1.pdf", width=12, height=4.5)
 par(cex.main = 1.5, lwd=2,
     mar = c(5, 4, 4, 2) + 0.1,
     mgp = c(3.5, 1, 0), 
-    cex.lab = 1.25, font.lab = 2, cex.axis = 1.2, bty = "l", las = 1, mfrow=c(3,2),
+    cex.lab = 1.25, font.lab = 2, cex.axis = 1.2, bty = "l", las = 1, mfrow=c(1,3),
     oma=c(3,3,0,0))
-p1b(t.results, main = "t-test", xlab="", ylab="", cex=0.25,
-    colours=rainbow(3, alpha=0.2), ylim =c(0,1))
-p1b(cor.results, main = "Correlation", 
-    legend=FALSE, xlab="", ylab="", cex=0.25,
-    colours=rainbow(3, alpha=0.2), ylim =c(0,1))
 p1b(anova.results, main = "ANOVA", 
     legend=FALSE, xlab="", ylab="", cex=0.25,
     colours=rainbow(3, alpha=0.2), ylim =c(0,1))
+legend("bottomright",c("NHT","IT", "IT, adjusted"), pch=19,
+       col=rainbow(3, alpha=0.5), bty="n", cex=1.2)
 p1b(lm.results, main = "Linear regression", 
     legend=FALSE, xlab="", ylab="", cex=0.25,
     colours=rainbow(3, alpha=0.2), ylim =c(0,1))
@@ -176,65 +172,50 @@ p1b(lm.colin.results, main = "Linear regression, collinearity",
     legend=FALSE, xlab="", ylab="", cex=0.25,
     colours=rainbow(3, alpha=0.2), ylim =c(0,1))
 mtext("Effect Size", side=1, outer=TRUE, cex=2, line=0)
-mtext("Proportion of rightfull conclusions", side=2, outer=TRUE, cex=2, line=0.5, las=0)
+mtext("Prop. rightfull conclusions", side=2, outer=TRUE, cex=2, line=0.5, las=0)
 dev.off()
 
 
 ## M-Errors
-pdf("../overleaf/figures/Sup_M-error.pdf", width=8, height=11)
+pdf("../overleaf/figures/Sup_M-error.pdf", width=12, height=4.5)
 par(cex.main = 1.5, lwd=2,
     mar = c(5, 4, 4, 2) + 0.1,
     mgp = c(3.5, 1, 0), 
-    cex.lab = 1.25, font.lab = 2, cex.axis = 1.2, bty = "l", las = 1, mfrow=c(3,2),
+    cex.lab = 1.25, font.lab = 2, cex.axis = 1.2, bty = "l", las = 1, mfrow=c(1,3),
     oma=c(3,3,0,0))
-p3b(t.results, main = "t-test", xlab="", ylab="", cex=0.25, xlim =c(0.1,4),
-    colours=rainbow(3, alpha=0.2),
-    log="xy",
-    pos.leg="topright")
-p3b(cor.results, main = "Correlation", 
-    legend=FALSE, xlab="", ylab="", cex=0.25, xlim =c(0.1,4),
-    colours=rainbow(3, alpha=0.2), log="xy")
 p3b(anova.results, main = "ANOVA", 
-    legend=FALSE, xlab="", ylab="", cex=0.25, xlim =c(0.1,4),
-    colours=rainbow(3, alpha=0.2), log="xy")
+    legend=FALSE, xlab="", ylab="", cex=0.25, xlim =c(0.1,3),
+    colours=rainbow(3, alpha=0.2))
+legend("topright",c("NHT","IT", "IT, adjusted"), pch=19,
+       col=rainbow(3, alpha=0.5), bty="n", cex=1.2)
 p3b(lm.results, main = "Linear regression", 
-    legend=FALSE, xlab="", ylab="", cex=0.25, xlim =c(0.1,4),
-    colours=rainbow(3, alpha=0.2), log="xy")
+    legend=FALSE, xlab="", ylab="", cex=0.25, xlim =c(0.1,3),
+    colours=rainbow(3, alpha=0.2))
 p3b(lm.colin.results, main = "Linear regression, collinearity",
-    legend=FALSE, xlab="", ylab="", cex=0.25, xlim =c(0.1,4),
-    colours=rainbow(3, alpha=0.2), log="xy")
+    legend=FALSE, xlab="", ylab="", cex=0.25, xlim =c(0.1,3),
+    colours=rainbow(3, alpha=0.2))
 mtext("Effect Size", side=1, outer=TRUE, cex=2, line=0)
 mtext("Mean M-error rate", side=2, outer=TRUE, cex=2, line=0.5, las=0)
 dev.off()
 
 ## S-Errors
-pdf("../overleaf/figures/Sup_S-error.pdf", width=8, height=11)
+pdf("../overleaf/figures/Sup_S-error.pdf", width=12, height=4.5)
 par(cex.main = 1.5, lwd=2,
     mar = c(5, 4, 4, 2) + 0.1,
     mgp = c(3.5, 1, 0), 
-    cex.lab = 1.25, font.lab = 2, cex.axis = 1.2, bty = "l", las = 1, mfrow=c(3,2),
+    cex.lab = 1.25, font.lab = 2, cex.axis = 1.2, bty = "l", las = 1, mfrow=c(1,3),
     oma=c(3,3,0,0))
-p4b(t.results, main = "t-test", xlab="", ylab="", cex=0.25,
-    ylim=c(1e-4,1), xlim =c(0.1,4),
-    colours=rainbow(3, alpha=0.2),
-    log="xy", pos.leg="bottomleft")
-p4b(cor.results, main = "Correlation", 
-    legend=FALSE, xlab="", ylab="", cex=0.25, xlim =c(0.1,4), ylim=c(1e-4,1) ,
-    colours=rainbow(3, alpha=0.2),
-    log="xy")
 p4b(anova.results, main = "ANOVA", 
-    legend=FALSE, xlab="", ylab="", cex=0.25, xlim =c(0.1,4), ylim=c(1e-4,1),
-    colours=rainbow(3, alpha=0.2),
-    log="xy")
+    legend=FALSE, xlab="", ylab="", cex=0.5, xlim =c(0.1,1.5),
+    colours=rainbow(3, alpha=0.5))
+legend("topright",c("NHT","IT", "IT, adjusted"), pch=19,
+       col=rainbow(3, alpha=0.5), bty="n", cex=1.2)
 p4b(lm.results, main = "Linear regression", 
-    legend=FALSE, xlab="", ylab="", cex=0.25, xlim =c(0.1,4), ylim=c(1e-4,1),
-    colours=rainbow(3, alpha=0.2),
-    log="xy")
+    legend=FALSE, xlab="", ylab="", cex=0.5, xlim =c(0.1,1.5),
+    colours=rainbow(3, alpha=0.5))
 p4b(lm.colin.results, main = "Linear regression, collinearity",
-    legend=FALSE, xlab="", ylab="", cex=0.25, xlim =c(0.1,4), ylim=c(1e-4,1),
-    colours=rainbow(3, alpha=0.2),
-    log="xy" )
+    legend=FALSE, xlab="", ylab="", cex=0.5, xlim =c(0.1,1.5),
+    colours=rainbow(3, alpha=0.5))
 mtext("Effect Size", side=1, outer=TRUE, cex=2, line=0)
 mtext("Proportion of S-error", side=2, outer=TRUE, cex=2, line=0.5, las=0)
 dev.off()
-
